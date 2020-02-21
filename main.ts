fe5d21dcb758d5858cb12ce75abd42dff4db4df0
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-02-21 07:41:40 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+input.onButtonPressed(Button.B, function () {
+    kitronik_servo_lite.stop()
+})
+input.onButtonPressed(Button.A, function () {
+    kitronik_servo_lite.forward()
+    basic.showArrow(ArrowNames.North)
+})
